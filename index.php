e4db2882dc483697938ccad670ddfaa7e9050fc5
Thibaut C <t.chevallier11@gmail.com>
2020-06-03 15:16:21 +0200

    Mise au propre partie patient, nouveau style, manque image en background

--- index.php
+++ index.php
@@ -1,84 +1,80 @@
 <?php
 session_start();
-
 if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['username']);
     header("location: index.php");
 }
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="Static/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="Static/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <meta charset="UTF-8">
-    <title>ALED - Medecine interventions</title>
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/nouislider/nouislider.css">
-    <!-- Google fonts - Playfair Display-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
-
-    <!-- owl carousel-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/owl.carousel/assets/owl.carousel.css">
-    <!-- Ekko Lightbox-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/ekko-lightbox/ekko-lightbox.css">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/css/custom.css">
-    <!-- Favicon-->
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/solid.css" integrity="sha384-TbilV5Lbhlwdyc4RuIV/JhD8NR+BfMrvz4BL5QFa2we1hQu6wvREr3v6XSRfCTRp" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/regular.css" integrity="sha384-avJt9MoJH2rB4PKRsJRHZv7yiFZn8LrnXuzvmZoD3fh1aL6aM6s0BBcnCvBe6XSD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/brands.css" integrity="sha384-7xAnn7Zm3QC1jFjVc1A6v/toepoG3JXboQYzbM0jrPzou9OFXm/fY6Z/XiIebl/k" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/fontawesome.css" integrity="sha384-ozJwkrqb90Oa3ZNb+yKFW2lToAWYdTiF1vt8JiH5ptTGHTGcN7qdoR1F95e0kYyG" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <link rel="shortcut icon" type="image/png" href="Static/images/doctor.png"/>
-    <link rel="stylesheet" href="Static/css/style.css">
-    <link rel="stylesheet" href="Static/css/normalize.css">
-    <link href="/Static/css/blog.css" rel="stylesheet">
+
+    <title>Aled</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="Static/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="Static/css/cover.css" rel="stylesheet">
 
 </head>
-<body>
 
-<div class="content">
-    <!-- notification message -->
+<body class="text-center">
 
-    <!-- logged in user information -->
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">Aled</h3>
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link active" href="index.php">Home</a>
+                <?php  if (isset($_SESSION['username'])) : ?>
+                    <a class="nav-link" href="#"><?php echo $_SESSION['username']; ?></a>
+                    <a class="nav-link" href="index.php?logout='1'">logout</a>
+                <?php else :?>
+                    <a class="nav-link" href="login.php">Login</a>
+                <?php endif ?>
+            </nav>
+        </div>
+    </header>
 
+    <main role="main" class="inner cover">
+        <?php  if (isset($_SESSION['username'])) : ?>
+            <h1 class="cover-heading">Make Your Diagnosis</h1>
+            <p class="lead">Aled is web and mobile application where you can make your own diagnosis. You can now make your own diagnosis using the button below</p>
+            <p class="lead">
+                <a href="presurvey/souris.html" class="btn btn-lg btn-secondary">Make diagnosis</a>
+            </p>
+        <?php else :?>
+            <h1 class="cover-heading">You need to register first !</h1>
+            <p class="lead">Aled is web and mobile application where you can make your own diagnosis. But first we need to know who you are ! So please, use the button below to register or login.</p>
+            <p class="lead">
+                <a href="login.php" class="btn btn-lg btn-secondary">Login now</a>
+            </p>
+        <?php endif ?>
+    </main>
 
-        <header class="blog-header py-3 px-3">
-            <div class="row flex-nowrap justify-content-between align-items-center mx-5">
-                <div class="col-4 pt-1">
-                    <a class="text-muted" href="presurvey/souris.html">Faire son diagnostic</a>
-                    <a class="text-muted ml-5" href="camera_upload.html">Test Caméra</a>
-                </div>
-                <div class="col-4 text-center">
-                    <a class="blog-header-logo text-dark" href="http://boushoku.alwaysdata.net">ALED</a>
-                </div>
-                <div class="col-4 d-flex justify-content-end align-items-center">
-
-                    <?php  if (isset($_SESSION['username'])) : ?>
-                        <strong class="mr-5">Welcome <?php echo $_SESSION['username']; ?></strong>
-                        <a class="btn btn-sm btn-outline-secondary" href="index.php?logout='1'" >logout</a>
-                    <?php else :?>
-                        <a class="btn btn-sm btn-outline-secondary" href="login.php">Login</a>
-                    <?php endif ?>
-
-                </div>
-            </div>
-        </header>
-
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>&copy; 2020 <a href="index.php">Aled</a>.  All rights reserved.</p>
+        </div>
+    </footer>
 </div>
 
-<?php
-    include "base.html";
-    include "footer.html";
-?>
 
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="Static/js/jquery.min.js"</script>
+<script>window.jQuery || document.write('<script src="Static/js/jquery.min.js"><\/script>')</script>
+<script src="Static/js/popper.min.js"></script>
+<script src="Static/js/bootstrap.min.js"></script>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,69 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="Static/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="Static/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <meta charset="UTF-8">
-    <title>ALED - Medecine interventions</title>
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/nouislider/nouislider.css">
-    <!-- Google fonts - Playfair Display-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700">
-
-    <!-- owl carousel-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/owl.carousel/assets/owl.carousel.css">
-    <!-- Ekko Lightbox-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/vendor/ekko-lightbox/ekko-lightbox.css">
-    <!-- theme stylesheet-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="https://d19m59y37dris4.cloudfront.net/sell/1-2-3/css/custom.css">
-    <!-- Favicon-->
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-    <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/solid.css" integrity="sha384-TbilV5Lbhlwdyc4RuIV/JhD8NR+BfMrvz4BL5QFa2we1hQu6wvREr3v6XSRfCTRp" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/regular.css" integrity="sha384-avJt9MoJH2rB4PKRsJRHZv7yiFZn8LrnXuzvmZoD3fh1aL6aM6s0BBcnCvBe6XSD" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/brands.css" integrity="sha384-7xAnn7Zm3QC1jFjVc1A6v/toepoG3JXboQYzbM0jrPzou9OFXm/fY6Z/XiIebl/k" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/fontawesome.css" integrity="sha384-ozJwkrqb90Oa3ZNb+yKFW2lToAWYdTiF1vt8JiH5ptTGHTGcN7qdoR1F95e0kYyG" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <link rel="shortcut icon" type="image/png" href="Static/images/doctor.png"/>
-    <link rel="stylesheet" href="Static/css/style.css">
-    <link rel="stylesheet" href="Static/css/normalize.css">
-    <link href="/Static/css/blog.css" rel="stylesheet">
+
+    <title>Aled</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="Static/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="Static/css/cover.css" rel="stylesheet">
 
 </head>
 <body>
-<style type="text/css">
-    .form-signin{
-        padding-left: 500px;
-        padding-right: 500px;
-        /*padding-top: 100px;*/
-    }
-</style>
 <?php
-    include "header.html";
-    include('server.php');
-    include('errors.php');
+include('server.php');
+include('errors.php');
 ?>
+<div class="cover-container d-flex h-100 p-3 mx-1 flex-column">
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">Aled</h3>
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link" href="index.php">Home</a>
+                <a class="nav-link active" href="login.php">Login</a>
+            </nav>
+        </div>
+    </header>
+    <main role="main" class="inner cover">
+        <a href="index.php"><img class="mt-5 mb-4 mx-auto d-block" src="Static/images/doctor.png" alt="" width="72" height="72"></a>
+        <h1 class="cover-heading text-center">Please log in</h1>
+        <p class="lead">Aled is web and mobile application where you can make your own diagnosis. But first we need to know who you are ! So please, use the form below to register or login.</p>
 
-<form class="form-signin text-center mt-5" method="post" action="login.php">
-    <img class="mb-4" src="Static/images/doctor.png" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Please log in</h1>
 
-        <label class="sr-only">Username</label>
-        <input type="text" name="username" class="form-control" placeholder="username" required autofocus>
+        <form class="form-signin text-center mt-5" method="post" action="login.php">
 
-        <label class="sr-only">Password</label>
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <label class="sr-only">Username</label>
+            <input type="text" name="username" class="form-control mb-3" placeholder="username" required autofocus>
 
-    <button type="submit" class="btn btn-lg btn-primary btn-block" name="login_user">Login</button>
-    <p><a class="p-2 text-dark" href="#">Forgot password</a> ?</p>
-    <p>Pas encore de compte ?<a class="p-2 text-dark" href="register.php">Inscrivez-vous</a> !</p>
-    <p class="mt-5 mb-3 text-muted">&copy; 2019-2020</p>
-</form>
+            <label class="sr-only">Password</label>
+            <input type="password" name="password" class="form-control mb-3" placeholder="Password" required>
 
+            <button type="submit" class="btn btn-lg btn-primary btn-block mb-5" name="login_user">Login</button>
+            <p><a class="p-2" href="#">Forgot password</a> ?</p>
+            <p>Not registered yet ?<a class="p-2" href="register.php">Create an account now</a> !</p>
+        </form>
+    </main>
+
+
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>&copy; 2020 <a href="index.php">Aled</a>.  All rights reserved.</p>
+        </div>
+    </footer>
+</div>
 </body>
 </html>
 
